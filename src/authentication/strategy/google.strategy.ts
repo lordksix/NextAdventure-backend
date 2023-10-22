b14037224fe0e300bccc01d1b451bdec23ed8c34
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-10-22 12:52:25 -0500

    Fix google oauth bug

--- src/authentication/strategy/google.strategy.ts
+++ src/authentication/strategy/google.strategy.ts
@@ -1,5 +1,5 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { Profile, Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
@@ -14,19 +14,20 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     });
   }
   async validate(
-    accessToken: string,
-    refreshToken: string,
-    profile: any,
+    req: any,
+    _accessToken: string,
+    _refreshToken: string,
+    profile: Profile,
     done: VerifyCallback,
   ): Promise<any> {
-    const { name, emails, photos } = profile;
+    const { name, emails, photos, id } = profile;
     const user = {
+      providerId: id,
       email: emails[0].value,
       firstName: name.givenName,
       lastName: name.familyName,
       picture: photos[0].value,
-      accessToken,
-      refreshToken,
+      provider: 'google',
     };
     done(null, user);
   }
