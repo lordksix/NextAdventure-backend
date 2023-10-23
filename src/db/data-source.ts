1d28d9e222059f016bc4990c77fa8bab8b16e725
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-10-23 18:38:00 -0500

    Implement database module

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,19 +1,33 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { config } from 'dotenv';
 import { SeederOptions } from 'typeorm-extension';
+import InitSeeder from './seeds/init.seeder';
 
 config();
 
-export const dataSourceOptions: DataSourceOptions & SeederOptions = {
-  type: 'postgres',
-  host: process.env.DATABASE_PG_HOST,
-  port: +process.env.DATABASE_PG_PORT,
-  username: process.env.DATABASE_PG_USERNAME,
-  password: process.env.DATABASE_PG_PASSWORD,
-  database: process.env.DATABASE_PG_NAME,
-  entities: ['dist/resource/**/*.entity.js'],
-  migrations: ['dist/db/migrations/*.js'],
-  seeds: ['dist/db/seeds/**/*.js'],
-};
+export const dataSourceOptions: DataSourceOptions & SeederOptions =
+  process.env.DIST === 'true'
+    ? {
+        type: 'postgres',
+        host: process.env.DATABASE_PG_HOST,
+        port: +process.env.DATABASE_PG_PORT,
+        username: process.env.DATABASE_PG_USERNAME,
+        password: process.env.DATABASE_PG_PASSWORD,
+        database: process.env.DATABASE_PG_NAME,
+        entities: ['dist/resource/**/*.entity{.ts,.js}'],
+        migrations: ['dist/db/migrations/*{.ts,.js}'],
+        seeds: [InitSeeder],
+      }
+    : {
+        type: 'postgres',
+        host: process.env.DATABASE_PG_HOST,
+        port: +process.env.DATABASE_PG_PORT,
+        username: process.env.DATABASE_PG_USERNAME,
+        password: process.env.DATABASE_PG_PASSWORD,
+        database: process.env.DATABASE_PG_NAME,
+        entities: ['src/resource/**/*.entity{.ts,.js}'],
+        migrations: ['dist/db/migrations/*{.ts,.js}'],
+        seeds: [InitSeeder],
+      };
 
 export default new DataSource(dataSourceOptions);
