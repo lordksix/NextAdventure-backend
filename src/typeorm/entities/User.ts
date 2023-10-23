48151b54137cd930195dd0592faddbf4c16014f5
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-10-23 02:33:38 -0500

    Setup user entity

---
+++ src/typeorm/entities/User.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ type: 'varchar', length: 50 })
+  email: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  firstName: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  lastName: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  provider: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  providerId: string;
+
+  @Column({ type: 'varchar', length: 512, default: '' })
+  refreshToken: string;
+}
