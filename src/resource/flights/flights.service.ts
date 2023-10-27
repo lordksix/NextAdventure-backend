87251668c3546d33f3a017bc54fc40f797a96bee
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-10-26 20:38:49 -0500

    Remove authenticantion from booking service

--- src/resource/flights/flights.service.ts
+++ src/resource/flights/flights.service.ts
@@ -4,6 +4,8 @@ import { Repository } from 'typeorm';
 import { Flight } from './entities/flight.entity';
 import { City } from '../cities/entities/city.entity';
 import { User } from '../users/entities/user.entity';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class FlightsService {
@@ -16,11 +18,11 @@ export class FlightsService {
     private userRepository: Repository<User>,
   ) {}
 
-  async findAll(arrivalId: number, depatureId: number) {
+  async findAll(arrivalId: number, departureId: number) {
     const flights = await this.flightRepository.find({
       where: {
         arrivalCity: await this.cityRepository.findOneBy({ id: arrivalId }),
-        departureCity: await this.cityRepository.findOneBy({ id: depatureId }),
+        departureCity: await this.cityRepository.findOneBy({ id: departureId }),
       },
     });
     return { data: flights };
@@ -31,12 +33,26 @@ export class FlightsService {
     return { data: [flight] };
   }
 
-  async update(id: number, refreshToken: string) {
+  async update(
+    id: number,
+    _refreshToken: string,
+    registerUserDto: CreateUserDto,
+  ) {
     try {
       const flight = await this.flightRepository.findOneBy({ id });
+      const { email, firstName, lastName } = registerUserDto;
+      console.log('first');
       if (flight.seatsAvailable < 1)
         return `No seats available for flight #${id}`;
-      const user = await this.userRepository.findOneBy({ refreshToken });
+      /* const user = await this.userRepository.findOneBy({ refreshToken }); */
+      const user = this.userRepository.create({
+        email,
+        firstName,
+        lastName,
+        provider: 'local',
+        providerId: randomUUID(),
+      });
+      console.log(user);
       await this.flightRepository.update(
         { id },
         {
@@ -44,6 +60,7 @@ export class FlightsService {
           seatsAvailable: flight.seatsAvailable - 1,
         },
       );
+      console.log('third');
       return `Boooking completed for flight #${id}`;
     } catch {
       return `Boooking COULDN'T BE completed for flight #${id}`;
--- src/resource/users/dto/create-user.dto.ts
+++ src/resource/users/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { IsRFC5321Email } from 'src/utils/email.validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsString({ message: 'Email must be a string' })
+  @MaxLength(50, { message: 'Maximum length of 50 chars' })
+  @IsRFC5321Email({ message: 'Invalid email addres' })
+  email: string;
+
+  @IsNotEmpty({ message: 'First name is required' })
+  @IsString({ message: 'First name must be a string' })
+  @MaxLength(50, { message: 'Maximum length of 50 chars' })
+  firstName: string;
+
+  @IsNotEmpty({ message: 'last name is required' })
+  @IsString({ message: 'Last name must be a string' })
+  @MaxLength(50, { message: 'Maximum length of 50 chars' })
+  lastName: string;
+}
